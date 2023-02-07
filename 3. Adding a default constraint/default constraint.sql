1d294606469113ca8e020b75ec370a09d25fc5a3
Alvin Kimata <alvinkimata@gmail.com>
2023-02-07 09:23:56 +0300

    delete a default constraint

--- 3. Adding a default constraint/default constraint.sql
+++ 3. Adding a default constraint/default constraint.sql
@@ -1,14 +1,14 @@
 Use [Sample1]
 
 
-/*Create Table tblPerson
+Create Table tblPerson
 (
 	ID int NOT NULL Primary Key,
 	Name nvarchar(50) NOT NULL,
 	Email nvarchar(50) NOT NULL,
 	GenderId int
 )
-*/
+
 
 Select * from tblGender
 Select * from tblPerson
@@ -19,3 +19,8 @@ Insert into tblPerson(ID, Name, Email) Values (8, 'Mike', 'mike@you.com');
 ALTER TABLE tblPerson
 ADD CONSTRAINT DF_tblPerson_GenderId
 Default 3 FOR GENDERID;
+
+
+--Drop a constraint.
+ALTER TABLE tblPerson
+DROP CONSTRAINT DF_tblPerson_GenderId
