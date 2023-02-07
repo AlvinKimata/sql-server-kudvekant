dac225e09093db7f76f5c955a09cbbf225ebcb61
Alvin Kimata <alvinkimata@gmail.com>
2023-02-07 09:04:19 +0300

    add a foreign key reference

--- 2. Managing tables/SQLQuery1.sql
+++ 2. Managing tables/SQLQuery1.sql
@@ -12,6 +12,9 @@ Select * from tblGender
 
 Insert into tblGender (ID, Gender) Values(7, 'Male')
 
+--Add a foreign key reference using a query.
+Add table tblGender Add constraint tblGender_ID FOREIGN_KEY (gender_ID) references tblGender(ID)
+
 --Add a default constraint for the gender.
 
 Alter Table tblGender Add constraint DF_tblGender_GenderID
