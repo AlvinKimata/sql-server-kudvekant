407496917d4ca07d5481fd17d2809d0c0a2a7ee3
Alvin Kimata <alvinkimata@gmail.com>
2023-02-07 10:04:43 +0300

    delete a default constraint

--- 5. Check constraint/check_constraint.sql
+++ 5. Check constraint/check_constraint.sql
@@ -2,3 +2,7 @@ Select * from tblPerson;
 
 Alter table tblPerson Add Age nvarchar(50);
 Insert into tblPerson values(5, 'Mary', 'mary@mail.com',3 ,-40)
+
+--Add a constraint to a row.
+Alter Table tblPerson
+Add Constraint CK_tblPerson_Age CHECK (Age > 0 AND Age < 150)
