47f0f95f8573d65ae5f47fa5bb570d554e7ee3bc
Alvin Kimata <alvinkimata@gmail.com>
2023-02-07 09:20:41 +0300

    add a default constraint

---
+++ 3. Adding a default constraint/default constraint.sql
@@ -0,0 +1,21 @@
+Use [Sample1]
+
+
+/*Create Table tblPerson
+(
+	ID int NOT NULL Primary Key,
+	Name nvarchar(50) NOT NULL,
+	Email nvarchar(50) NOT NULL,
+	GenderId int
+)
+*/
+
+Select * from tblGender
+Select * from tblPerson
+
+Insert into tblPerson(ID, Name, Email) Values (8, 'Mike', 'mike@you.com');
+
+--Add a default constraint.
+ALTER TABLE tblPerson
+ADD CONSTRAINT DF_tblPerson_GenderId
+Default 3 FOR GENDERID;
